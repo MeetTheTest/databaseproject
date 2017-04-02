3e26e2104f24502beaed2ac49cab9703894a87dc
Ruslan Kuleshov <Russlegreyson@gmail.com>
2017-04-02 14:30:10 +0300

    middle of commit

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,1 +1,80 @@
 INSERT INTO developers VALUES (1, 'Ivan Ivanov');
+Insert INTO developers VALUES (2, 'Mike Johnson');
+INSERT INTO developers VALUES (3, 'George Commit');
+INSERT into developers VALUES (5, 'Uri Boiko');
+INSERT into developers VALUES (6, 'David Dawn');
+INSERT into developers VALUES (7, 'Tom  Dogh');
+INSERT into developers VALUES (8, 'Bart Simpson');
+
+
+INSERT  INTO skills VALUES (1, 'Java');
+INSERT  INTO skills VALUES (2, 'SQL');
+INSERT INTO skills values (3, 'C++');
+INSERT INTO skills VALUES (4, 'UI/UX');
+INSERT INTO skills VALUES (5, 'html/css');
+INSERT INTO skills VALUES (6, 'JavaScript');
+
+INSERT INTO projects VALUES (1, 'UI/UX design');
+INSERT INTO projects VALUES (2, 'Trade-stock bot');
+INSERT INTO projects VALUES (3, 'Bank app');
+INSERT INTO projects VALUES (4, 'Database for website');
+
+INSERT INTO Companies VALUES (1, 'T-Systems');
+INSERT INTO Companies VALUES (2, 'MoneySoft');
+INSERT INTO Companies VALUES (3, 'DevelopIt');
+
+
+INSERT INTO customers VALUES (1, 'Alpha-Bank');
+INSERT INTO customers VALUES (2, 'Bulls Trade');
+INSERT INTO customers VALUES (3, 'Amazing Realty agency');
+INSERT INTO CUSTOMERS VALUES (4, 'Barbershop');
+
+
+INSERT INTO developer_skills VALUES (1, 1);
+INSERT INTO developer_skills VALUES (1, 2);
+
+INSERT INTO developer_skills VALUES (2, 3);
+INSERT INTO developer_skills VALUES (2, 2);
+
+INSERT INTO developer_skills VALUES (3, 1);
+INSERT INTO developer_skills VALUES (3, 4);
+INSERT INTO developer_skills VALUES (3, 5);
+INSERT INTO developer_skills VALUES (3, 6);
+
+INSERT INTO developer_skills VALUES (4, 1);
+INSERT INTO developer_skills VALUES (4, 2);
+
+INSERT INTO developer_skills VALUES (5, 4);
+INSERT INTO developer_skills VALUES (5, 5);
+
+INSERT INTO developer_skills VALUES (6, 6);
+INSERT INTO developer_skills VALUES (6, 2);
+
+INSERT INTO developer_skills VALUES (7, 1);
+INSERT INTO developer_skills VALUES (7, 2);
+
+
+
+
+INSERT INTO developer_PROJECT VALUES (1, 3);
+INSERT INTO developer_PROJECT VALUES (2, 2);
+INSERT INTO developer_PROJECT VALUES (3, 3);
+INSERT INTO developer_PROJECT VALUES (4, 2);
+INSERT INTO developer_PROJECT VALUES (5, 1);
+INSERT INTO developer_PROJECT VALUES (6, 4);
+INSERT INTO developer_PROJECT VALUES (7, 3);
+
+
+INSERT INTO developer_company VALUES (7, 3);
+
+
+
+INSERT INTO customer_orders VALUES (1, 2, 1);
+INSERT INTO customer_orders VALUES (2, 1, 2);
+INSERT INTO customer_orders VALUES (3, 4, 3);
+INSERT INTO customer_orders VALUES (4, 3, 4);
+
+
+
+
+
--- src/main/resources/database/tasks/1.sql
+++ src/main/resources/database/tasks/1.sql
@@ -1,1 +1,10 @@
-SELECT * FROM developers;
+ALTER TABLE Developers
+  ADD Salary INT NOT NULL;
+
+INSERT INTO Developers (id, salary) values (1,'2000$');
+INSERT INTO Developers (id, salary) values (2,'3000$');
+INSERT INTO Developers (id, salary) values (3,'8000$');
+INSERT INTO Developers (id, salary) values (4,'4000$');
+INSERT INTO Developers (id, salary) values (5,'3300$');
+INSERT INTO Developers (id, salary) values (6,'2500$');
+INSERT INTO Developers (id, salary) values (7,'6000$');
